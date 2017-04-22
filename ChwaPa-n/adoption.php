ccce9cbc7b0b66f3867a7c96d72968aae957989a
Mandy <amandaalcindor@gmail.com>
2017-04-22 12:43:44 -0700

    Modification page adoption

--- ChwaPa-n/adoption.php
+++ ChwaPa-n/adoption.php
@@ -10,7 +10,7 @@ require_once ("tete.php");
 					<div class="upper-box">
 						<div class="row">
 							<div class="col-md-6">
-								<h1>Adoption <span></span><i class="fa fa-question-circle"></i></h1>
+								<h1>Adoption </h1>
 								<p>L'adoption peut avoir pour but de pourvoir aux besoins d'un enfant par l'établissement d'une filiation alors qu'il en est dépourvu car il est orphelin. L'adoption peut aussi permettre à une personne, l'adoptant de transmettre son héritage car elle n'a pas d'enfant. Il peut aussi être question de la volonté de créer une famille. odio sit amet nibh vulputate cursus a sit amet mauris. </p>
 								<p>Selon le droit local, l'adoption peut être dite simple, plénière, confidentielle ou ouverte voire internationale si l'adopté ne provient pas du même pays que l'adoptant. À signaler, le droit musulman qui ne reconnait pas l'adoption mais permet une autre procédure : la kafala. Le droit positif peut faire coexister ces différents systèmes, ou n'en reconnaitre que certains. </p>
 								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -43,15 +43,133 @@ require_once ("tete.php");
 							</div>
 						</div>
 					</div>
-					<div class="down-box">
-						<ul class="device-list">
-							<li><img alt="" src="images/device1.png"></li>
-							<li class="device2"><img alt="" src="images/device2.png"></li>
-							<li class="device3"><img alt="" src="images/device3.png"></li>
-							<li class="device4"><img alt="" src="images/device4.png"></li>
-						</ul>
+
+				
+			<!-- Product Box -->
+			<div class="product-box">
+				<div class="container">
+					<h2>Les enfants sont adorables, protégeons les.</h2>
+					<div id="carousel-example-generic4" class="carousel slide" data-ride="carousel">
+
+						<!-- Wrapper for slides -->
+						<div class="carousel-inner">
+
+							<div class="item active">
+								<div class="row">
+									<div class="col-md-3">
+										<div class="product-post">
+											<div class="product-post-gal">
+												<img alt="" src="upload/en1.jpg">
+											</div>											
+										</div>
+									</div>
+
+									<div class="col-md-3">
+										<div class="product-post">
+											<div class="product-post-gal">
+												<img alt="" src="upload/en2.jpg">
+											</div>
+											
+										</div>
+									</div>
+
+									<div class="col-md-3">
+										<div class="product-post">
+											<div class="product-post-gal">
+												<img alt="" src="upload/en3.jpg">
+											</div>
+											
+										</div>
+									</div>
+
+									<div class="col-md-3">
+										<div class="product-post">
+											<div class="product-post-gal">
+												<img alt="" src="upload/enfant1.jpg">
+											</div>
+											
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="item">
+								<div class="row">
+									<div class="col-md-3">
+										<div class="product-post">
+											<div class="product-post-gal">
+												<img alt="" src="upload/enfant2.jpg">
+											</div>
+											
+										</div>
+									</div>
+
+									<div class="col-md-3">
+										<div class="product-post">
+											<div class="product-post-gal">
+												<img alt="" src="upload/en1.jpg">
+											</div>
+											
+										</div>
+									</div>
+
+									<div class="col-md-3">
+										<div class="product-post">
+											<div class="product-post-gal">
+												<img alt="" src="upload/en3.jpg">
+											</div>
+											
+										</div>
+									</div>
+
+									<div class="col-md-3">
+										<div class="product-post">
+											<div class="product-post-gal">
+												<img alt="" src="upload/en2.jpg">	
+											</div>
+											
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+						<!-- Controls -->
+						<a class="left carousel-control" href="#carousel-example-generic4" data-slide="prev"></a>
+						<a class="right carousel-control" href="#carousel-example-generic4" data-slide="next"></a>
 					</div>
 				</div>
+			</div>
+				</div>
+			</div>
+
+			<div class="services-box">
+				<div class="container">
+					<div class="row">
+
+						<div class="col-md-4">
+							<div class="services-post">
+								<a class="services-icon1" href="#"><i class="fa fa-users"></i></a>
+								<div class="services-post-content">
+									<h4>Adoptez moi</h4>
+									<p>Cliquez ici pour adopter</p>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-md-4">
+							<div class="services-post">
+								<a class="services-icon2" href="#"><i class="fa fa-users"></i></a>
+								<div class="services-post-content">
+									<h4>Je veux donner en adoption</h4>
+									<p>Cliquer ici pour donner en adoption</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<img class="shadow-image" alt="" src="images/shadow.png">
 			</div>
 
 
