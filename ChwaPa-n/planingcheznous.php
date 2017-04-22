fac98dc5cac26e81416db17a55c3e7525032489d
v3ll13 <laguerre.vellie@gmail.com>
2017-04-21 21:49:01 -0400

    decoupage du site

---
+++ ChwaPa-n/planingcheznous.php
@@ -0,0 +1,148 @@
+<?php
+require_once ("tete.php");
+?>
+
+<!-- partners box -->
+			<div class="partners">
+				<div class="container">
+					<h3>Le planning familial en Haiti</h3>
+					<div id="carousel-example-generic2" class="carousel slide" data-ride="carousel">
+
+						<!-- Wrapper for slides -->
+						<div class="carousel-inner">
+
+							<div class="item active">
+								<ul class="partner-list">
+									<li><img alt="" src="images/partners.png"></li>
+									<li><img alt="" src="images/partners.png"></li>
+									<li><img alt="" src="images/partners.png"></li>
+									<li><img alt="" src="images/partners.png"></li>
+									<li><img alt="" src="images/partners.png"></li>
+								</ul>
+							</div>
+
+							<div class="item">
+								<ul class="partner-list">
+									<li><img alt="" src="images/partners.png"></li>
+									<li><img alt="" src="images/partners.png"></li>
+									<li><img alt="" src="images/partners.png"></li>
+									<li><img alt="" src="images/partners.png"></li>
+									<li><img alt="" src="images/partners.png"></li>
+								</ul>
+							</div>
+
+						</div>
+
+						<!-- Controls -->
+						<a class="left carousel-control" href="#carousel-example-generic2" data-slide="prev"></a>
+						<a class="right carousel-control" href="#carousel-example-generic2" data-slide="next"></a>
+					</div>
+					
+				</div>
+			</div>
+
+			<!-- Latest Post -->
+			<div class="latest-post">
+				<div class="title-section">
+					<h1>Fok nou renmen tet nou ak peyi nou</h1>
+					<p>Pensons pour notre pays, pensons pour notre famille car il est de notre devoir de contribuer au progres dee notre pays. Prenons donc une decision "SMART"</p>
+				</div>
+				
+				 	<h1>C'est quoi un planning</i></h1>
+						<p>Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. <a href="#">Duis sed</a> odio sit amet nibh vulputate cursus a sit amet mauris. </p>
+						<p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. </p>
+						<p>Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. <a href="#">Duis sed</a> odio sit amet nibh vulputate cursus a sit amet mauris. </p>
+						<p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. </p>
+					<h1>Pourquoi le faire</i></h1>
+						<p>Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. <a href="#">Duis sed</a> odio sit amet nibh vulputate cursus a sit amet mauris. </p>
+						<p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. </p>
+						<p>Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. <a href="#">Duis sed</a> odio sit amet nibh vulputate cursus a sit amet mauris. </p>
+						<p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. </p>
+										
+				</div>
+			</div>
+
+			<!-- WHY convertible box -->
+			<div class="why-convertible-box">
+				<div class="container">
+					<div class="upper-box">
+						<div class="row">
+							<div class="col-md-6">
+								<h1>Necessite du planning familial en Haiti<i class="fa fa-exclamation-circle"></i></h1>
+								<p>Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. <a href="#">Duis sed</a> odio sit amet nibh vulputate cursus a sit amet mauris. </p>
+								<p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. </p>
+							</div>
+							
+						</div>
+					</div>
+					<div class="down-box">
+						<ul class="device-list">
+							<li><img alt="" src="images/device1.png"></li>
+							<li class="device2"><img alt="" src="images/device2.png"></li>
+							<li class="device3"><img alt="" src="images/device3.png"></li>
+							<li class="device4"><img alt="" src="images/device4.png"></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			
+
+			<div class="infographic-box">
+				<div class="container">
+					<h3>Statistiques</h3>
+					<div class="skills-bar">
+						<div class="row">
+							<div class="col-md-2">
+								<div id="circle" data-percent="50"></div>
+								<p>Grossesse non desire</p>
+							</div>
+							<div class="col-md-2">
+								<div id="circle2" data-percent="39"></div>
+								<p>Nombre d'enfant minimal par famille</p>
+							</div>
+							<div class="col-md-2">
+								<div id="circle3" data-percent="90"></div>
+								<p>Circular Progress Bar</p>
+							</div>
+							<div class="col-md-2">
+								<div id="circle4" data-percent="60"></div>
+								<p>Circular Progress Bar</p>
+							</div>
+							<div class="col-md-2">
+								<div id="circle5" data-percent="80"></div>
+								<p>Circular Progress Bar</p>
+							</div>
+							<div class="col-md-2">
+								<div id="circle6" data-percent="45"></div>
+								<p>Circular Progress Bar</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		
+
+						<!-- Controls -->
+						<a class="left carousel-control" href="#carousel-example-generic4" data-slide="prev"></a>
+						<a class="right carousel-control" href="#carousel-example-generic4" data-slide="next"></a>
+					</div>
+				</div>
+			</div>
+
+			<!-- Convertible-banner -->
+			<div class="convertible-banner">
+				<div class="container">
+					<a href="#">Learn More</a>
+					<p><span>Convertible</span> The most complete PSD Template with pixel perfect design available only on themeforest</p>
+				</div>
+			</div>
+
+		
+		
+		<!-- End content -->
+
+
+	</div>
+	<!-- End Container -->
---
+++ ChwaPa-n/tete.php
@@ -0,0 +1,114 @@
+
+<!doctype html>
+
+<html lang="en" class="no-js">
+
+<head>
+	<title>Chwa pan'</title>
+
+	<meta charset="utf-8">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,700,600,300' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
+    <!-- REVOLUTION BANNER CSS SETTINGS -->
+    <link rel="stylesheet" type="text/css" href="css/fullwidth.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="css/settings.css" media="screen" />
+	
+	<link rel="stylesheet" type="text/css" href="css/magnific-popup.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="css/owl.carousel.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="css/owl.theme.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="css/jquery.bxslider.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css" media="screen">
+
+
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery.migrate.js"></script>
+	<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="js/raphael-min.js"></script>
+	<script type="text/javascript" src="js/DevSolutionSkill.min.js"></script>
+	<script type="text/javascript" src="js/retina-1.1.0.min.js"></script>
+	<script type="text/javascript" src="js/jquery.bxslider.min.js"></script>
+	<script type="text/javascript" src="js/plugins-scroll.js"></script>
+
+     <!-- jQuery KenBurn Slider  -->
+    <script type="text/javascript" src="js/jquery.themepunch.revolution.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	
+</head>
+<body>
+
+	<!-- Container -->
+	<div id="container">
+		<!-- Header
+		    ================================================== -->
+		<header class="clearfix">
+			<!-- Static navbar -->
+			<div class="navbar navbar-default navbar-fixed-top">
+				<div class="top-line">
+					<div class="container">
+						<p>
+							<span><i class="fa fa-phone"></i>1234 - 5678 - 9012</span>
+							<span><i class="fa fa-envelope-o"></i>support@convertible.com</span>
+						</p>
+						<ul class="social-icons">
+							<li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a class="rss" href="#"><i class="fa fa-rss"></i></a></li>
+							<li><a class="google" href="#"><i class="fa fa-google-plus"></i></a></li>
+							<li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+							<li><a class="pinterest" href="#"><i class="fa fa-pinterest"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#"><img alt="" src="images/logo.png"></a>
+					</div>
+					<div class="navbar-collapse collapse">
+						<ul class="nav navbar-nav navbar-right">
+
+							<li><a class="active" href="index.php">Acceuil</a>
+			
+							</li>
+							<li class="drop"><a href="menu.html">Menu</a>
+								<ul class="drop-down">
+									<li><a href="planingcheznous.php">Planning chez nous et dans le monde</a></li>
+									<li><a href="flexslider.html">Planning et religion</a></li>
+									<li><a href="flexslider.html">Methode de planning</a></li>
+								</ul>
+							</li>
+							<li><a href="about.html">A propos</a></li>
+							<li class="drop"><a href="blog-right-sidebar.html">Forum</a>
+								<ul class="drop-down">
+									<li><a href="blog-one-col.html">Blog 1col</a></li>
+									<li><a href="blog-two-col.html">Blog 2col</a></li>
+									<li><a href="blog-right-sidebar.html">Blog right sidebar</a></li>
+									<li><a href="blog-left-sidebar.html">Blog left sidebar</a></li>
+									<li><a href="blog-nosidebar.html">Blog no sidebar</a></li>
+								</ul>
+							</li>						
+							<li><a href="contact.html">Contactez-nous</a></li>
+
+						</ul>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- End Header -->
