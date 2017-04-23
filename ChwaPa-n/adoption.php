f3c9c5b1c90a6bc2f97dcdde42299b50f188f6ed
Thodly <thodlydugue@gmail.com>
2017-04-22 22:21:46 -0400

    preparation pitch

--- ChwaPa-n/adoption.php
+++ ChwaPa-n/adoption.php
@@ -11,7 +11,7 @@ require_once ("tete.php");
 						<div class="row">
 							<div class="col-md-6">
 								<h1>Adoption </h1>
-								<p>L'adoption peut avoir pour but de pourvoir aux besoins d'un enfant par l'établissement d'une filiation alors qu'il en est dépourvu car il est orphelin. L'adoption peut aussi permettre à une personne, l'adoptant de transmettre son héritage car elle n'a pas d'enfant. Il peut aussi être question de la volonté de créer une famille. odio sit amet nibh vulputate cursus a sit amet mauris. </p>
+								<p>L'adoption peut avoir pour but de pouvoir aux besoins d'un enfant par l'établissement d'une filiation alors qu'il en est dépourvu car il est orphelin. L'adoption peut aussi permettre à une personne, l'adoptant de transmettre son héritage car elle n'a pas d'enfant. Il peut aussi être question de la volonté de créer une famille. odio sit amet nibh vulputate cursus a sit amet mauris. </p>
 								<p>Selon le droit local, l'adoption peut être dite simple, plénière, confidentielle ou ouverte voire internationale si l'adopté ne provient pas du même pays que l'adoptant. À signaler, le droit musulman qui ne reconnait pas l'adoption mais permet une autre procédure : la kafala. Le droit positif peut faire coexister ces différents systèmes, ou n'en reconnaitre que certains. </p>
 								
 								<p>Foire internationale si l'adopté ne provient pas du même pays que l'adoptant. À signaler, le droit musulman qui ne reconnait pas l'adoption mais permet une autre procédure :Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
